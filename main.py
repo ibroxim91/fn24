38ee33e280bddcb7f423828dcd6403856bec4027
ibrohim <ibrohim@gmail.com>
2024-07-22 15:26:32 +0500

    Add all files

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+Lesson 3   Closures Git and Github
+"""
+
+
+# global
+x = 15
+
+# if 1 == 22:
+#     # local
+#     a = 15
+#
+a = 0
+
+
+# def calculate(a, b):
+#     def command():
+#         pass
+#
+#     def plus():
+#         return a + b
+#
+#     def minus():
+#         return a - b
+#
+#     command.plus = plus
+#     command.minus = minus
+#     return command
+#
+#
+# res = calculate(10, 15)
+# print(res.plus())
+# print(res.minus())
